6f0fa09c6087c836e914ce982440c2cf500c6b28
MFreit50 <michaelfreitas.mf50@gmail.com>
2026-02-10 22:53:47 -0500

    Create formatted_print_consumer.py
    prints event logs in a more readable format for the console

---
+++ src/consumers/examples/formatted_print_consumer.py
@@ -0,0 +1,18 @@
+from src.core.contracts.consumer import Consumer
+from src.core.contracts.event import Event
+from pprint import pprint
+
+class FormattedPrintConsumer(Consumer):
+    """
+    Example consumer that prints event details to the console
+    in a formatted manner.
+    """
+
+    def on_event(self, event: Event) -> None:
+        """
+        Handles a single event emitted by a producer by printing its details.
+        Args:
+            event (Event): The event to be handled
+        """
+        pprint(event.to_dict())
+        print()
