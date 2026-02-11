00f684b0b461cc37d9723dade46260ce7a29e14f
MFreit50 <michaelfreitas.mf50@gmail.com>
2026-02-10 22:52:46 -0500

    Removed event buffer from InMemoryTransport
    in memory transport is no longer responsible for storing events and strictly published events to consumers

--- src/transport/in_memory/in_memory_transport.py
+++ src/transport/in_memory/in_memory_transport.py
@@ -1,20 +1,15 @@
-from typing import List
-
 from src.core.contracts.event import Event
 from src.transport.base.base_transport import BaseTransport
 
 class InMemoryTransport(BaseTransport):
     """
     In-memory transport mechanism for moving events from producers to consumers.
-
-    - Stores events in memory
     - Delivers events to all subscribed consumers immediately upon publishing
 
     """
 
     def __init__(self) -> None:
         super().__init__()
-        self._events: List[Event] = []
 
     def publish(self, event: Event) -> None:
         """
@@ -25,11 +20,4 @@ class InMemoryTransport(BaseTransport):
             InvalidEventError: if the event is invalid.
         """
 
-        self._events.append(event)
-
         super().publish(event)
-
-    @property
-    def events(self) -> List[Event]:
-        """Read-only access to the list of stored events."""
-        return self._events.copy()
