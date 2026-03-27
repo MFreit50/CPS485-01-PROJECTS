300f6ba5ac9b27cd5311c5ec429a8ba4ec796d54
MFreit50 <michaelfreitas.mf50@gmail.com>
2026-03-27 00:00:46 -0400

    Demo scripts utilize updated apis
    demo scripts have been updated to be compatible with updated apis following implementation of identifiable objects

--- demo/async_demo.py
+++ demo/async_demo.py
@@ -2,17 +2,23 @@ import asyncio
 
 from src.consumers.examples.print_consumer import PrintConsumer
 from src.consumers.examples.save_to_db import SaveToDBConsumer
-from src.core.contracts.event import Event
+from src.core.events.base.base_event import BaseEvent
 from src.transport.in_memory.in_memory_transport import InMemoryTransport
 
 
 async def main():
     transport = InMemoryTransport(number_of_workers=1)
-    transport.subscribe(PrintConsumer())
-    transport.subscribe(SaveToDBConsumer())
+    print_consumer = PrintConsumer()
+    db_consumer = SaveToDBConsumer()
+    transport.subscribe(print_consumer)
+    transport.subscribe(db_consumer)
+
+    print(print_consumer)
+    print(db_consumer)
+
     await transport.start()
     for i in range(5):
-        event = Event(0, "test")
+        event = BaseEvent(0, "test")
         await transport.publish(event)
     await transport.flush()
 
